b65dfb0b836eca8bf7f5309ac9061bc504a3ea9f
Joe G <wheeltirefamily@gmail.com>
2014-01-02 07:42:06 -0600

    3.5 exercise 1 - Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
   describe "Home page" do
 
